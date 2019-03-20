92908815466cdfe65227f49e71c7087097e17dfa
Beau Hicken <Turbodiesel2500dee@hotmail.com>
2019-03-20 09:09:49 -0600

    update controllers

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,48 +1,48 @@
 class Api::ItemsController < ApplicationController
-  # before_action :set_department
-  # before_action :set_item, only: [:create, :update, :destroy]
+  before_action :set_department
+  before_action :set_item, only: [:create, :update, :destroy]
 
-  # def index
-  #   render json: @department.items.all
-  # end
+  def index
+    render json: @department.items.all
+  end
 
-  # def show
-  #   render json: @department.item
-  # end
+  def show
+    render json: @department.item
+  end
 
-  # def create
-  #   item = Item.new(item_params)
-  #   if item.save
-  #     render json: item
-  #   else
-  #     render json: item.errors, status: 422
-  #   end
+  def create
+    item = Item.new(item_params)
+    if item.save
+      render json: item
+    else
+      render json: item.errors, status: 422
+    end
 
-  # end
+  end
 
-  # def update
-  #   if @item.update(item_params)
-  #     render json: @item
-  #   else
-  #     render json: @item.errors, status: 422
-  #   end
+  def update
+    if @item.update(item_params)
+      render json: @item
+    else
+      render json: @item.errors, status: 422
+    end
 
-  # end
+  end
 
-  # def destroy
-  #   @item.destroy
-  # end
+  def destroy
+    @item.destroy
+  end
 
-  # private
-  # def item_params
-  #   @item = @department.item.find(params[:id])
-  # end
+  private
+  def item_params
+    @item = @department.item.find(params[:id])
+  end
 
-  # def set_item
-  #   params.require(:item).permit(:name, :description, :price)
-  # end
+  def set_item
+    params.require(:item).permit(:name, :description, :price)
+  end
 
-  # def set_department
-  #   @department = Department.find(params[:department_id])
-  # end
+  def set_department
+    @department = Department.find(params[:department_id])
+  end
 end
